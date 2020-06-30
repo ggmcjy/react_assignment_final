16f7158d8f2d51fce0907d5432ce000b51150bf2
unknown <ggmcjy94@naver.com>
2020-06-30 15:32:13 +0900

    end

--- src/components/screens/Video.js
+++ src/components/screens/Video.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
+import style2 from '../../assets/styles/Home.style';
+import Header from '../commons/Header';
 export default class Video extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isLoading: true,
+      data: [],
     };
   }
-
+  componentDidMount = () =>{
+    
+  }
   render() {
+    const { data } = this.state;
     return (
-      <View>
-        <Text> Video </Text>
+      <View style={style2.container}>
+        <Header />
+        <View style={style2.section}>
+
+        </View>
       </View>
     );
   }
